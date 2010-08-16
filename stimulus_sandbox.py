fb11be0f2c90daf4e133ab230c3cae4148e9786c
arokem <arokem@berkeley.edu>
2010-08-15 19:26:32 -0700

    Stimulus in sand box almost done. Need to figure out how to draw the target at arbitrary contrast together with all the rest of it.

--- stimulus_sandbox.py
+++ stimulus_sandbox.py
@@ -27,6 +27,7 @@ surround_inner = 0.4
 
 ring_width = 0.1
 spoke_width = 0.1
+num_spokes = 8
 
 win = visual.Window((600,600.0),
                     allowGUI=False,
@@ -34,9 +35,7 @@ win = visual.Window((600,600.0),
                     units='deg')
 
 outer_surround  = visual.PatchStim(win,tex="sin",mask="circle",texRes=128,
-                                   color=[1.0,1.0,1.0],
-                                   colorSpace='rgb', opacity=1.0,
-                                   contrast = surround_contrast,
+                                   color=surround_contrast,
                                    size=(surround_outer-ring_width/2,
                                          surround_outer-ring_width/2),
                                    sf=(spatial_freq,spatial_freq),
@@ -44,60 +43,60 @@ outer_surround  = visual.PatchStim(win,tex="sin",mask="circle",texRes=128,
 
 annulus = visual.PatchStim(win,tex="sin",mask="circle",texRes=128,
                            color=annulus_contrast,
-                           colorSpace='rgb',
-                           opacity=1.0,
                            size=(annulus_outer-ring_width/2,
                                  annulus_outer-ring_width/2),
                            sf=(spatial_freq,spatial_freq),
                            ori = annulus_ori)
 
 inner_surround = visual.PatchStim(win,tex="sin",mask="circle",texRes=128,
-                                  color=[1.0,1.0,1.0],
-                                  colorSpace='rgb',
-                                  opacity=1.0,
-                                  contrast=surround_contrast,
+                                  color=surround_contrast,
                                   size=(annulus_inner-ring_width/2,
                                         annulus_inner-ring_width/2),
                                   sf=(spatial_freq,spatial_freq),
                                   ori = surround_ori)
 
 #This is the bit between the annulus and the outer surround: 
-ring1 = visual.RadialStim(win, tex=None, color=[-1,-1,-1],
+ring1 = visual.RadialStim(win, tex=None, color=-1,
                           size=[annulus_outer+ring_width/2,
                                annulus_outer+ring_width/2],
-                          units='deg',
                           visibleWedge=[0, 360],
                           radialCycles=4, angularCycles=8,
-                          interpolate=True,contrast = 1)
+                          interpolate=True)
 
 #This is the bit between the annulus and the inner surround: 
-ring2 = visual.RadialStim(win, tex=None, color=[-1,-1,-1],
+ring2 = visual.RadialStim(win, tex=None, color=-1,
                           size=[annulus_inner+ring_width/2,
                                 annulus_inner+ring_width/2],
-                          units='deg',
                           visibleWedge=[0, 360],
                           radialCycles=4, angularCycles=8,
-                          interpolate=True,contrast = 1)
+                          interpolate=True)
 
 #This is the central area, between the inner surround and the fixation: 
 center_area = visual.RadialStim(win, tex=None, color=0,
                           size=[surround_inner,
                                 surround_inner],
-                          units='deg',
                           visibleWedge=[0, 360],
                           radialCycles=4, angularCycles=8,
-                          interpolate=True,contrast = 1)
-
+                          interpolate=True)
 
 target_wedge = visual.RadialStim(win, tex = None, color=0,
                                  opacity = 0.5,
-                                 size = [annulus_outer,annulus_outer],
-                                 visibleWedge=[0, 45],
-                                 ) 
-#fixation = visual.RadialStim(win,tex ='sin' fixation_tex)
+                                 size = [annulus_outer-ring_width,
+                                         annulus_outer-ring_width],
+                                 visibleWedge=[0, 45])
 
-#Setting the tex argument to None sets this to just be uniform: 
-center = visual.PatchStim(win,tex=None) 
+spokes = []
+for i in np.arange(num_spokes/2):
+    spokes.append(visual.ShapeStim(win,
+                         fillRGB = -1,
+                         lineRGB = -1,
+                         vertices = ((-spoke_width/2,annulus_outer/2),
+                                     (spoke_width/2,-annulus_outer/2),
+                                     (-spoke_width/2,-annulus_outer/2),
+                                     (spoke_width/2,annulus_outer/2)),
+                         ori=i*45))
+
+#fixation = visual.RadialStim(win,tex ='sin' fixation_tex)
 
 trial_clock = core.Clock()
 t = lastFPSupdate = 0
@@ -112,8 +111,10 @@ while t<duration:#sets the duration
     outer_surround.draw()
     ring1.draw()
     annulus.draw()  
-    ring2.draw()
     target_wedge.draw()
+    for spoke in spokes:
+        spoke.draw()
+    ring2.draw()
     inner_surround.draw()
     center_area.draw()
     
