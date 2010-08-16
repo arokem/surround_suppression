047e2afc17cd3e4c28075a452e68f96a28e417f1
arokem <arokem@berkeley.edu>
2010-08-15 22:30:20 -0700

    Stimulus including fixation and target at arbitrary contrast. Need to add fixation action.

--- stimulus_sandbox.py
+++ stimulus_sandbox.py
@@ -8,7 +8,7 @@ from psychopy import visual,core
 
 #For now, set these params locally, eventually move to the params file: 
 
-duration = 10
+duration = 5
 
 #For the grating:
 temporal_freq = 4
@@ -18,37 +18,43 @@ surround_ori = 0
 annulus_ori = 90
 
 surround_contrast = 1.
-annulus_contrast = 0.5
+annulus_contrast = 0.8
+target_contrast = 0.1
 
 surround_outer = 12.2
 annulus_outer = 6
 annulus_inner = 3
 surround_inner = 0.4
 
+fixation_size = 0.2
+
 ring_width = 0.1
 spoke_width = 0.1
 num_spokes = 8
 
-win = visual.Window((600,600.0),
+target_loc = 4
+
+win = visual.Window((800,800),
                     allowGUI=False,
                     monitor='testMonitor',
                     units='deg')
+    
 
-outer_surround  = visual.PatchStim(win,tex="sin",mask="circle",texRes=128,
+outer_surround  = visual.PatchStim(win,tex="sin",mask="circle",texRes=256,
                                    color=surround_contrast,
                                    size=(surround_outer-ring_width/2,
                                          surround_outer-ring_width/2),
                                    sf=(spatial_freq,spatial_freq),
                                    ori = surround_ori)
 
-annulus = visual.PatchStim(win,tex="sin",mask="circle",texRes=128,
+annulus = visual.PatchStim(win,tex="sin",mask="circle",texRes=256,
                            color=annulus_contrast,
                            size=(annulus_outer-ring_width/2,
                                  annulus_outer-ring_width/2),
                            sf=(spatial_freq,spatial_freq),
                            ori = annulus_ori)
 
-inner_surround = visual.PatchStim(win,tex="sin",mask="circle",texRes=128,
+inner_surround = visual.PatchStim(win,tex="sin",mask="circle",texRes=256,
                                   color=surround_contrast,
                                   size=(annulus_inner-ring_width/2,
                                         annulus_inner-ring_width/2),
@@ -56,34 +62,42 @@ inner_surround = visual.PatchStim(win,tex="sin",mask="circle",texRes=128,
                                   ori = surround_ori)
 
 #This is the bit between the annulus and the outer surround: 
-ring1 = visual.RadialStim(win, tex=None, color=-1,
+ring1 = visual.PatchStim(win, tex=None, mask='circle', color=-1,
                           size=[annulus_outer+ring_width/2,
                                annulus_outer+ring_width/2],
-                          visibleWedge=[0, 360],
-                          radialCycles=4, angularCycles=8,
-                          interpolate=True)
+                         interpolate=True)
 
 #This is the bit between the annulus and the inner surround: 
-ring2 = visual.RadialStim(win, tex=None, color=-1,
+ring2 = visual.PatchStim(win, tex=None, mask='circle', color=-1,
                           size=[annulus_inner+ring_width/2,
                                 annulus_inner+ring_width/2],
-                          visibleWedge=[0, 360],
-                          radialCycles=4, angularCycles=8,
                           interpolate=True)
 
 #This is the central area, between the inner surround and the fixation: 
-center_area = visual.RadialStim(win, tex=None, color=0,
+center_area = visual.PatchStim(win, tex=None, mask='circle', color=0,
                           size=[surround_inner,
                                 surround_inner],
-                          visibleWedge=[0, 360],
-                          radialCycles=4, angularCycles=8,
                           interpolate=True)
 
-target_wedge = visual.RadialStim(win, tex = None, color=0,
-                                 opacity = 0.5,
-                                 size = [annulus_outer-ring_width,
-                                         annulus_outer-ring_width],
-                                 visibleWedge=[0, 45])
+#Generate a mask, which will cover everything except for the target wedge:
+grid_array = np.linspace(-1*annulus.size[0],annulus.size[0],annulus.texRes)
+x,y=np.meshgrid(grid_array,grid_array)
+r = np.sqrt(x**2 + y**2)
+theta = np.arctan2(x,y) + np.pi
+target_mask = np.ones((annulus.texRes,annulus.texRes))
+target_mask[np.where(r>annulus_outer)] = -1
+target_mask[np.where(r<annulus_inner)] = -1
+target_mask[np.where(theta<target_loc*np.deg2rad(45))] = -1
+target_mask[np.where(theta>(target_loc+1)*np.deg2rad(45))]=-1
+
+#Now show the target contrast in the wedge:
+target_wedge = visual.PatchStim(win,tex="sin",mask=target_mask,
+                                texRes=256,
+                                color=target_contrast,
+                                size=(annulus_outer-ring_width/2,
+                                annulus_outer-ring_width/2),
+                                sf=(spatial_freq,spatial_freq),
+                                ori = annulus_ori)
 
 spokes = []
 for i in np.arange(num_spokes/2):
@@ -96,7 +110,14 @@ for i in np.arange(num_spokes/2):
                                      (spoke_width/2,annulus_outer/2)),
                          ori=i*45))
 
-#fixation = visual.RadialStim(win,tex ='sin' fixation_tex)
+# Fixation (made out of two concentric squares):
+fixation = visual.PatchStim(win, tex=None, color=1,
+                            size=fixation_size,
+                            interpolate=True)
+
+fixation_center = visual.PatchStim(win, tex=None, color=-1,
+                            size=fixation_size/2,
+                            interpolate=True)
 
 trial_clock = core.Clock()
 t = lastFPSupdate = 0
@@ -106,6 +127,7 @@ while t<duration:#sets the duration
     annulus.setContrast(np.sin(t*temporal_freq*np.pi*2))
     inner_surround.setContrast(np.sin(t*temporal_freq*np.pi*2))    
     outer_surround.setContrast(np.sin(t*temporal_freq*np.pi*2))    
+    target_wedge.setContrast(np.sin(t*temporal_freq*np.pi*2))
 
     #Draw them (order matters!)
     outer_surround.draw()
@@ -117,7 +139,9 @@ while t<duration:#sets the duration
     ring2.draw()
     inner_surround.draw()
     center_area.draw()
-    
+    fixation.draw()
+    fixation_center.draw()
+
     win.flip() #update the screen
 
 win.close()
