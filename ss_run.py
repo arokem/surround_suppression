cc58a4ad3f25d8e4ed58aa90effb37fcdcff7b2d
arokem <arokem@berkeley.edu>
2010-08-23 14:50:12 -0700

    Added logic for the different tasks

--- ss_run.py
+++ ss_run.py
@@ -11,7 +11,6 @@ import numpy as np
 from ss_classes import Params
 from psychopy import gui
 
-
 #This brings in all of the classes defined in ss_classes:
 from ss_classes import *
     
@@ -23,33 +22,64 @@ if __name__ == "__main__":
     #For some reason, if this call is inside ss_classes, just importing
     #ss_classes starts an instance of the GUI, so we put it out here:
     app = wx.App()
-    params.set_by_gui()
     app.MainLoop()
+    params.set_by_gui()
     params.target_ori = params.annulus_ori
     
     #This initializes the window (for now, this is just a part of monitor 0):
     win = visual.Window([800,600],monitor='testMonitor',units='deg')
 
-    staircase = Staircase(params.start_target_contrast,
-                          params.annulus_contrast/params.contrast_increments)
-
+    #Make a trial list:
     trial_list = [Trial(win,params,0),Trial(win,params,1),Trial(win,params,2)]
+
+    #Initialize the staircase, depending on which task is performed
+    if params.task == 'Annulus':
+        staircase = Staircase(params.start_target_contrast,
+                            params.annulus_contrast/params.contrast_increments,
+                            harder = 1, #For this task, higher values are
+                                      #actually harder => closer to the annulus
+                                      #value
+                            ub=params.target_contrast_max,
+                            lb=params.target_contrast_min
+                            )
+        fix_target_co = np.ones(len(trial_list)) * params.fix_target_start
+    
+    elif params.task == 'Fixation':
+        staircase = Staircase(params.fix_target_start,
+                            params._contrast/params.contrast_increments,
+                            harder = 1, 
+                            ub=params.fix_target_max,
+                            lb=params.fix_target_min
+                            )
+        target_co = np.ones(len(trial_list)) * params.start_target_contrast
+        
 
     #Send a message to the screen and wait for a subject keypress:
     start_text(win) 
         
     #Loop over the event list, while consuming each event, by calling it:
-    
-    for this_trial in trial_list:
-        #
-        this_trial.stimulus.finalize(params,target_co=staircase.value,
-                                     target_loc=0)
-        this_trial.stimulus(duration=params.stimulus_duration)
-        this_trial.fixation(duration=0.01) 
+    for trial_idx,this_trial in enumerate(trial_list):
+        if params.task=='Annulus':
+            this_trial.stimulus.finalize(params,target_co=staircase.value,
+                                         target_loc=0,fix_target_loc=1,
+                                         fix_target_co=fix_target_co[trial_idx])
+
+        elif params.task=='Fixation':
+            this_trial.stimulus.finalize(params,target_co=target_co[trial_idx],
+                                         target_loc=0,fix_target_loc=1,
+                                         fix_target_co=staircase.value)
+
+        this_trial.stimulus()
+        this_trial.fixation()
+
+        
         this_trial.response.finalize(correct_key = '1')
         correct = this_trial.response()
+
         this_trial.feedback.finalize(correct)
         this_trial.feedback()
+
         staircase.update(correct)
 
     win.close()
+    
