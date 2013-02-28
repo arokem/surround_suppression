56e8c2cccb9ccdf45e29e3f6c0b386bee8ccb62a
Summer Sheremata <ssheremata@berkeley.edu>
2013-02-28 13:20:02 -0500

    added files for experiment (ss_run_2afc.py) in which subjects decide whether target appeared on left or right. Also, added parameter to determine keys corresponding to left or right target

---
+++ ss_params_2afc.py
@@ -0,0 +1,74 @@
+
+"""
+    This file stores all of the a priori variables for the ss program. All
+    variables must be stored inside of the p dict.
+
+"""
+
+p = dict(
+    paradigm = 'block',          #'rapid_fire',     # 'block' for the fmri
+                                 #block design 
+                                 #(trials_per_block of trials in which the
+                                 #subject performs the task, followed by
+                                 #trials_per_block of no-task trials or
+                                 #'rapid_fire' for a continuous stream of task
+                                 #trials. 
+    monitor = 'NNL',     # 'NNL','582J_multisync'
+    screen = 0,                  # 0 is for the primary screen, 1 for auxillary
+    fullscreen =False,           # True if fullscreen, False otherwise 
+    scanner = True,              # True if the stimulus presentation should be
+                                 # triggered by a scanner ttl pulse
+    scanner_wait_time = 4, #extra seconds at beginning of scan, 
+                                          # will present fixation point, rings and spokes 
+    start_target_contrastA = 0.5, # Where to start the staircase for parallel condition.
+    start_target_orthog_contrastA = 0.25, #Where to start the staircase for orthogonal condition.  This will make sure that staircases converge on threshold.
+    start_target_contrastB = 0.1, # Where to start the staircase.
+    fix_target_start = 0.2,     # Where to start the staircase.    
+    fix_baseline = 0.2, #baseline contrast of fixation (to compare with fix_target_start)
+    display_units = 'deg',       # 'deg' means all the units below are in
+                                 # degrees of visual angle.
+    annulus_inner = 2*3,           # deg of visual angle, Default: 2.86
+    annulus_outer = 2*6,           # deg of visual angle, Default: 7.8
+    annulus_contrast = 0.2,     # relative contrast, Default: 0.2
+    surround_outer = 2*12.2,       # deg of visual angle, Default: 12.2
+    surround_inner = 2*1.0,        # deg of visual angle, must be larger than fixation_size 
+    surround_contrast = 0.8,     # relative contrast, Default: 0.75
+    ring_width = 2*0.1,            # deg of visual angle, Default: 0.1
+    spoke_width =  2*0.1,          # deg of visual angle, Default: 0.1
+    spatial_freq = 1.1,          # cycles/deg, Default: 1.1
+    spatial_phase = 0,           # seconds, Default: 0
+    temporal_freq = 4,           # Hz, Default: 4
+    temporal_phase = 0,          # seconds, Default: 0
+    stimulus_duration = 0.75,    # seconds, Default: 0.75
+    fixation_duration = 0.1,     # seconds, Default: 0.1
+    response_duration = 0.9,     # seconds, Default: 0.9
+    feedback_duration = 0.25,    # seconds, Default: 0.25
+    fixation_size = 1.2,         # deg of visual angle, Standard: 1.2
+    contrast_increments = 15,    # How many steps from the lowest to the highest
+                                 #contrast, Standard: 15 
+    targetA_contrast_max = 1.0,
+    targetB_contrast_max = 1.0,
+    targetB_contrast_min = 0.001,
+    fix_target_max = 1,          #
+    fix_target_min = 0,       #
+    trials_per_dummy = 5, #only have 5 trials in original block, dummy block that is not analyzed
+    trials_per_block = 15,        #
+    num_blocks = 8,             # Number of trials will be num_blocks *
+                                 # trials_per_block 
+    dummy_blocks = 1,            # In 'block' mode, the number of dummy blocks
+                                 # at the beginning of the run.
+    left_key = 1,
+    right_key = 2,
+    )
+
+#This should be the same:
+p['targetA_contrast_min'] = p['annulus_contrast']
+p['targetB_contrast_max'] = p['annulus_contrast']
+#This is derived from the above settings: 
+p['trial_duration'] =  (p['stimulus_duration'] +
+                        p['fixation_duration'] +
+                        p['response_duration'] +
+                        p['feedback_duration'])
+
+
+p['block_duration'] = p['trials_per_block'] * p['trial_duration']
---
+++ ss_run_2afc.py
@@ -0,0 +1,168 @@
+"""
+Surround suppression experiment, based on Zenger-Landolt and Heeger (2003)
+
+And on the Psychtoolbox version used to get the data in Yoon et al. (2009 and
+2010).
+
+""" 
+import gc
+
+import wx
+import numpy as np
+from ss_classes_2afc import Params
+from psychopy import core,event
+import psychopy.monitors.calibTools as calib
+
+
+#This brings in all of the classes defined in ss_classes:
+from ss_classes_2afc import *
+from ss_tools import start_data_file
+    
+if __name__ == "__main__":
+    """ The main function. This actually runs the experiment """
+
+    #Initialize params from file:
+    params = Params()
+    #For some reason, if this call is inside ss_classes, just importing
+    #ss_classes starts an instance of the GUI, so we put it out here:
+    app = wx.App()
+    app.MainLoop()
+    params.set_by_gui()
+    
+    f = start_data_file(params.subject)
+
+    #Start by saving in the parameter setting:
+    params.save(f)
+    
+    #For now, assume that the target and the annulus are going to have the same
+    #orientation: 
+    params.target_ori = params.annulus_ori
+    
+    #This initializes the window:
+
+    #This is the folder where we keep the calibration data:
+    calib.monitorFolder = './calibration/'# over-ride the usual setting of
+                                          # where monitors are stored
+    mon = calib.Monitor(params.monitor) #Get the monitor object and pass that
+                                        #as an argument to win:
+
+    
+    win = visual.Window(monitor=mon,
+                        screen=params.screen,
+                        fullscr=params.fullscreen,
+                        units=params.display_units)
+
+    #Initialize all the instances of stimulus bits and pieces for reuse: 
+    bank = StimulusBank(win,params)
+
+    #Make a trial list:
+    trial_list = make_trial_list(win,params)
+
+    #Initialize the staircase, depending on which task is performed
+    if params.task == 'Annulus':
+        message = """ Where is the target ?\n Press 1 for  left, 2 for  right,\n  Press any key to start""" 
+        if params.surround_ori == params.annulus_ori:
+            staircaseA = Staircase(params.start_target_contrastA,
+                            params.annulus_contrast/params.contrast_increments,
+                            harder = -1, #For this task, higher values are
+                                      #actually harder => closer to the annulus
+                                      #value
+                            ub=params.targetA_contrast_max,
+                            lb=params.targetA_contrast_min)
+        else:
+            staircaseA = Staircase(params.start_target_orthog_contrastA,
+                            params.annulus_contrast/params.contrast_increments,
+                            harder = -1, #For this task, higher values are
+#                                      #actually harder => closer to the annulus
+                                      #value
+                            ub=params.targetA_contrast_max,
+                            lb=params.targetA_contrast_min)            
+        staircaseB = Staircase(params.start_target_contrastB,
+                            params.start_target_contrastB/params.contrast_increments,
+                            harder = -1, #For this task, higher values are
+                                      #actually harder => closer to the annulus
+                                      #value
+                            ub=params.targetB_contrast_max,
+                            lb=params.targetB_contrast_min)
+
+        if params._replay is None:
+            #The fixation target appears but has a constant contrast set to the
+            #starting point 
+            other_contrast = np.ones(len(trial_list)) * params.fix_target_start
+        else:
+            #Replay a previous run
+            other_contrast = params._replay
+            
+    elif params.task == 'Fixation':
+        message = """ Where is the target ?\n Press 1 for  left, 2 for  right,\n  Press any key to start""" 
+        #Just one staircase:
+        staircaseA = staircaseB = Staircase(params.fix_target_start,
+                            params.fix_target_start/params.contrast_increments,
+                            harder = -1, 
+                            ub=params.fix_target_max,
+                            lb=params.fix_target_min
+                            )
+
+        if params._replay is None:
+            #The annulus target appears and has a constant contrast set to the
+            #starting point: 
+            other_contrast = np.ones(len(trial_list)) * params.fix_target_start
+        else:
+            #Replay a previous run:
+            other_contrast = params._replay
+
+
+    #Send a message to the screen and wait for a subject keypress:
+    Text(win,text=message,height=0.7)() 
+
+    win.flip()
+    
+    #If this is in the scanner, we would want to wait for the ttl pulse right
+    #here:
+    if params.scanner:
+       Text(win,text='',keys=['t'])() #Assuming a TTL is a 't' key
+       Beginning(win,params,bank)       
+    #Loop over the event list, while consuming each event, by calling it:
+    for trial_idx,this_trial in enumerate(trial_list):
+        trial_clock = core.Clock()
+        if this_trial.block_type == 'A':
+            this_trial.finalize_stim(params,bank,staircaseA,other_contrast[trial_idx])
+        elif this_trial.block_type == 'B':
+            this_trial.finalize_stim(params,bank,staircaseB,other_contrast[trial_idx])
+        this_trial.stimulus(params,this_trial.block_type)
+        if this_trial.block_type == 'A':
+            this_trial.finalize_fix(params,bank,staircaseA,other_contrast[trial_idx])
+        if this_trial.block_type == 'B':
+            this_trial.finalize_fix(params,bank,staircaseB,other_contrast[trial_idx])
+        this_trial.fixation(params,this_trial.block_type)
+        this_trial.response.finalize(correct_key = this_trial.correct_key,
+                                     file_name=f)
+        this_trial.response()
+        if this_trial.correct_key is not None:
+            this_trial.feedback.finalize(this_trial.response.correct)
+        this_trial.feedback()
+#        print 'new trial'
+        #On the first trial, insert the header: 
+        if trial_idx == 0:
+            this_trial.save(f,insert_header=True)
+        else:
+            #On other trials, just insert the data:
+            this_trial.save(f)
+
+        #update after saving:
+        if this_trial.block_type == 'A':
+            staircaseA.update(this_trial.response.correct)
+        elif this_trial.block_type == 'B':
+            staircaseB.update(this_trial.response.correct)
+
+        this_trial.wait_iti(trial_clock)
+
+        #Flush unwanted events that may still be hanging out: 
+        event.clearEvents()
+
+        #dbg
+        # trial_clock.getTime()
+    
+    f.close()
+    core.quit()
+    
